be61a591eef3da773fa25dbcd529532e0fb4f99c
Italo Coppolella <italo7094@hotmail.it>
2025-11-18 00:24:04 +0100

    migliorata la logica. Inoltre ora preleva a, b direttamente dal form

--- 11-17/calcolatrice.js
+++ 11-17/calcolatrice.js
@@ -1,23 +1,36 @@
-let a = Number(prompt("Inserisci il primo numero"));
-let b = Number(prompt("Inserisci il secondo numero"));
+function eseguiCalcoli() {
+  // Prende i valori dai form
+  let a = Number(document.getElementById("num1").value);
+  let b = Number(document.getElementById("num2").value);
 
-function somma(a, b) {
-  return a + b;
+  // se a & b sono nulli non esegue i calcoli
+  if (a == "" || b == "") {
+    alert("Hai lasciato i campi vuoti!");
+  } else {
+    
+    function somma() {
+      return a + b;
+    }
+
+    function sottrazione() {
+      return a - b;
+    }
+
+    function prodotto() {
+      return a * b;
+    }
+
+    function divisione() {
+      if (a < b) {
+        alert("Il dividendo non può essere inferiore al divisore!");
+      } else {
+        return a / b;
+      }
+    }
+
+    console.log("La somma è", somma());
+    console.log("La sottrazione è", sottrazione());
+    console.log("Il prodotto è", prodotto());
+    console.log("La divisione è", divisione());
+  }
 }
-
-function sottrazione(a, b) {
-  return a - b;
-}
-
-function prodotto(a, b) {
-  return a * b;
-}
-
-function divisione(a, b) {
-  return a / b;
-}
-
-console.log(somma(a, b));
-console.log(sottrazione(a, b));
-console.log(prodotto(a, b)); 
-console.log(divisione(a, b));
